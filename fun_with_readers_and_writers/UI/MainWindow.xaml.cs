e0a6e08050ab10f626d47d493c2d65d88a3c99ba
Jonathan Christian <jchristian>
2012-01-08 19:42:21 -0800

    Added a solution that plays with xaml readers and writers.

---
+++ fun_with_readers_and_writers/UI/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xaml;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var reader = new XamlXmlReader("./Button.xaml"))
+            using (var writer = new XamlXmlWriter(new FileStream("./Test.xaml", FileMode.Create), reader.SchemaContext))
+            {
+                while (reader.Read())
+                {
+                    writer.WriteNode(reader);
+                }
+            }
+
+            using (var reader = new XamlObjectReader(new Button()))
+            using (var writer = new XamlObjectWriter(reader.SchemaContext))
+            {
+                while (reader.Read())
+                {
+                    writer.WriteNode(reader);
+                }
+
+                var button = (Button)writer.Result;
+            }
+
+            using (var reader = new XamlXmlReader(new StringReader("<Button xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\">This is a button</Button>")))
+            using (var writer = new XamlObjectWriter(reader.SchemaContext))
+            {
+                while (reader.Read())
+                {
+                    writer.WriteNode(reader);
+                }
+
+                var button = (Button)writer.Result;
+            }
+        }
+    }
+}
