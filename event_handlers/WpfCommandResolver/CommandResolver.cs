a5059f64996a4b5b194a66f7c419524dd6a3f8dc
Jonathan Christian <jchristian>
2012-01-08 19:04:53 -0800

    First commit

---
+++ event_handlers/WpfCommandResolver/CommandResolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace WpfCommandResolver
+{
+    public class CommandResolver : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (RoutedEventHandler)((sender, args) => new ClickCommandHandler().Handle());
+        }
+    }
+
+    public class ClickCommandHandler
+    {
+        public void Handle()
+        {
+            Debug.WriteLine("Handled");
+        }
+    }
+}
---
+++ event_handlers/event_handlers/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace event_handlers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var button = (Button)XamlReader.Load(new FileInfo("CommandButton.xaml").OpenRead());
+            ((Grid) FindName("TheGrid")).Children.Add(button);
+        }
+    }
+}
