13d5cd9d3e5b1069548f693f676dc3c5f032f8dc
U-Jonathan-UberPC\Jonathan <Jonathan@Jonathan-UberPC.(none)>
2012-01-16 13:57:06 -0800

    Added a solution for attached properties.

---
+++ attached_properties/UI/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var button = new Button{Content = "This is a button"};
+            var textBlock = new TextBlock{Text="Test"};
+
+            button.SetValue(Grid.ColumnProperty, 0);
+            button.SetValue(Grid.RowProperty, 0);
+            textBlock.SetValue(Grid.ColumnProperty, 1);
+            textBlock.SetValue(Grid.RowProperty, 0);
+
+            TheGrid.Children.Add(button);
+            TheGrid.Children.Add(textBlock);
+
+            TheOrderedStackPanel.Order();
+        }
+    }
+}
---
+++ attached_properties/UI/OrderedStackPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UI
+{
+    public class OrderedStackPanel : StackPanel
+    {
+        public static DependencyProperty OrderProperty = DependencyProperty.RegisterAttached("Order", typeof(int), typeof(OrderedStackPanel));
+
+        public static void SetOrder(DependencyObject element, int value)
+        {
+            element.SetValue(OrderProperty, value);
+        }
+
+        public static int GetOrder(DependencyObject element)
+        {
+            return (int)element.GetValue(OrderProperty);
+        }
+
+        public void Order()
+        {
+            var orderDictionary = new Dictionary<int, UIElement>();
+
+            foreach (var child in Children.OfType<UIElement>().ToList())
+            {
+                orderDictionary.Add(GetOrder(child), child);
+                Children.Remove(child);
+            }
+
+            foreach (var child in orderDictionary.OrderBy(x => x.Key))
+                Children.Add(child.Value);
+        }
+    }
+}
