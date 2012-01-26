443ea85fb73b63f730e17c2a810667193444e639
U-BENCHMARKPLUS\JChristian <JChristian@CHRISTIANDT.BenchmarkPlus.local>
2012-01-25 16:23:05 -0800

    Added a solution for messing with panels and layout.

---
+++ panels_and_such/UI/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using UI.canvas;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void CanvasButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            new Canvas().ShowDialog();
+        }
+    }
+}
