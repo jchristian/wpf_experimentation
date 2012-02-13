0411074f50a92a15c30c430bf2e25574983e1656
U-BENCHMARKPLUS\JChristian <JChristian@CHRISTIANDT.BenchmarkPlus.local>
2012-02-13 12:59:12 -0800

    Added a solution for messing with commands.

---
+++ commands/UI/CustomCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace UI
+{
+    public class CustomCommand : ICommand
+    {
+        public string MyCustomString { get { return "Custom Command!"; } }
+
+        public void Execute(object parameter)
+        {
+            Debug.WriteLine("The custom command was executed!");
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ commands/UI/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var a = ApplicationCommands.Help;
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Help, HelpExecuted, HelpCanExecute));
+            InputBindings.Add(new KeyBinding(ApplicationCommands.Help, new KeyGesture(Key.D, ModifierKeys.Alt)));
+        }
+
+        void HelpCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Help me!");
+        }
+    }
+}
