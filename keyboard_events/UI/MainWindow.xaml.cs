1de65e89ca3b3c59da1f270c578ccbebdf619904
unknown <Jonathan@Jonathan-UberPC.(none)>
2012-02-23 20:27:59 -0800

    .

---
+++ keyboard_events/UI/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TheStackPanel.KeyDown += OnKeyDown;
+            TheFirstTextBox.KeyDown += OnKeyDown;
+            TheLabel.KeyDown += OnKeyDown;
+            TheSecondTextBox.KeyDown += OnKeyDown;
+        }
+
+        public void OnKeyDown(object sender, KeyEventArgs args)
+        {
+            if (args.KeyboardDevice.IsKeyDown(Key.A) &&
+                args.KeyboardDevice.IsKeyDown(Key.LeftAlt))
+                Debug.WriteLine("");
+        }
+    }
+}
